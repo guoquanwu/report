e10d0070001b3c40e38437bf58329473f0674221
ray.wu <wgq.quan@gmail.com>
2020-12-17 17:44:31 +0800

    check binance fee fund

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module report
 go 1.13
 
 require (
-	github.com/guoquanwu/tool-kits v0.0.0-20201213091938-3853b0af4f4c
 	github.com/robfig/cron v1.2.0
-	github.com/wechaty/go-wechaty v0.2.1
+	github.com/urfave/cli/v2 v2.3.0
+	gopkg.in/yaml.v2 v2.2.3
 )
--- report.go
+++ report.go
@@ -6,18 +6,28 @@ import (
 	"errors"
 	"fmt"
 	"github.com/robfig/cron"
+	"github.com/urfave/cli/v2"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"report/binance"
 	"strconv"
 	"time"
 )
 
-var webhookUrl string = "" // slack webhook
+var conf Config
+
+type Config struct {
+	Webhook              string  `json:"webhook" yaml:"webhook"`
+	FundingRateThreshold float64 `json:"fundingRateThreshold" yaml:"fundingRateThreshold"`
+	Spec                 string  `json:"spec" yaml:"spec"`
+}
 
 type Response struct {
-	Symbol string `json:"symbol"`
-	FundingTime int64 `json:"fundingTime"`
+	Symbol      string `json:"symbol"`
+	FundingTime int64  `json:"fundingTime"`
 	FundingRate string `json:"fundingRate"`
 }
 
@@ -26,19 +36,79 @@ type SlackRequestBody struct {
 }
 
 func main() {
-	//spec := "0 10 0 * * *" // 10:00 everyday
-	spec := "0 * * * * *"  // every minutes
-	c := cron.New()
-	c.AddFunc(spec, check)
-	c.Start()
-	select {
+
+	var confPath string
+
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "config, c",
+				Usage:       "the config path",
+				Required:    true,
+				Destination: &confPath,
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "run",
+				Usage: "start report",
+				Action: func(c *cli.Context) error {
+					if confPath == "" {
+						fmt.Errorf("config cannot be null")
+					} else {
+						_, err := initConf(confPath)
+						if err != nil {
+							panic(err.Error())
+						}
+						run()
+					}
+					return nil
+				},
+			},
+		},
 	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func initConf(path string) (Config, error) {
+	yamlFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		err = fmt.Errorf("read config file in yaml style failure, error:%v", err)
+		return conf, err
+	}
+	err = yaml.Unmarshal(yamlFile, &conf)
+	if err != nil {
+		err = fmt.Errorf("parse yaml file to struct failure, error:%v", err)
+		return conf, err
+	}
+	return conf, nil
+}
+
+func run() {
+	//spec := "0 10 0 * *" // 10:00 everyday
+	//spec := "0 * * * *" // every minutes
+	//c := cron.New(cron.WithChain(
+	//	cron.Recover(cron.DefaultLogger), // or use cron.DefaultLogger
+	//))
+	c := cron.New()
+	c.AddFunc(conf.Spec, check)
+	c.Start()
+	select {}
+}
+
+func test() {
+	fmt.Println("haha")
 }
 
 func check() {
 	fmt.Println("start")
-	url := binance.Servers+binance.BTCFundingRate
-	response ,err := http.Get(url)
+	url := binance.Servers + binance.BTCFundingRate
+	response, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -52,12 +122,12 @@ func check() {
 		return
 	}
 	fmt.Println(result[0].FundingRate)
-	fundingRate, err := strconv.ParseFloat(result[0].FundingRate,64)
+	fundingRate, err := strconv.ParseFloat(result[0].FundingRate, 64)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	if fundingRate > 0 {
+	if fundingRate < conf.FundingRateThreshold {
 		send(fundingRate)
 	}
 }
@@ -70,14 +140,14 @@ func send(funding_rate float64) error {
 func SendSlackNotification(msg string) error {
 	slackBody, _ := json.Marshal(SlackRequestBody{Text: msg})
 	fmt.Println(string(slackBody))
-	req, err := http.NewRequest(http.MethodPost, webhookUrl, bytes.NewBuffer(slackBody))
+	req, err := http.NewRequest(http.MethodPost, conf.Webhook, bytes.NewBuffer(slackBody))
 	if err != nil {
 		return err
 	}
 
 	req.Header.Add("Content-Type", "application/json")
 
-	client := &http.Client{Timeout: 10 * time.Second}
+	client := &http.Client{Timeout: 2 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
